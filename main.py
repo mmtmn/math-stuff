b9c664d2434c5e5bc09f86cf110c2493dde85708
mmtmn <42742390+mmtmn@users.noreply.github.com>
2022-01-12 21:28:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 class Universe:
-    def __init__(self,stuff,*args):
-        self.stuff = stuff
+    def __init__(self,delta_v):
+        self.delta_v = delta_v # change in velocity
 
     print("Always executed")
     
     if __name__ == "__main__":
-        def StandardModel():
-            pass
+        def StandardModel(self):
+            Lsm = (-0.5 * self.delta_v)
+            return Lsm
     else:
         def Relativity():
             pass
+
+universe = Universe(1)
+print(universe.StandardModel())
